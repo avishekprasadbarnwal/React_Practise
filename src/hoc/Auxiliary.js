8e3fb9e36be846b27da6fbf921adc0bbad853b6e
Avishek <avishekprasadbarnwal@gamil.com>
2021-08-12 07:42:52 +0530

    react Hook and its working behind the scenes

---
+++ src/hoc/Auxiliary.js
@@ -0,0 +1,6 @@
+// import React from 'react';
+// As we are not using any JSX code or any react code particularly hence we dont need to import React in this case
+
+const Auxiliary = (props) => props.children;
+
+export default Auxiliary;
